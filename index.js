4d62281a02ed037d2fed3a2c146404b74cfc28af
armoraz <oawind@gmail.com>
2022-10-14 00:57:38 -0300

    change from force to alter

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const port = process.env.PORT || 3001;
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(port, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
