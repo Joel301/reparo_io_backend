51fc0895aeaa5716480d94fcb7e2e46c7d338eca
Joel301 <41971614+Joel301@users.noreply.github.com>
2022-10-13 21:37:39 -0500

    added port
    added port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./src/app.js");
-
-server.listen(3001, () => {
+const port = Process.env.PORT || 3000;
+server.listen(port, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
 });
