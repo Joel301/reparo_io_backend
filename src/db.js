020561b324cf8de4ef8074d411af691484fc4189
Omar Perez <42899316+armoraz@users.noreply.github.com>
2022-10-14 00:00:14 -0300

    Updated db

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
   //`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/reparoio`,
-  postgres://postgres:RGdeXILyEnk1CVNUbrZW@containers-us-west-33.railway.app:6106/railway
+  `postgresql://postgres:RGdeXILyEnk1CVNUbrZW@containers-us-west-33.railway.app:6106/railway`
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
