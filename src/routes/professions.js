7b8fec9c6c7a69622001fd7add16190794b40920
hectordsol <hectordsol@gmail.com>
2022-10-14 02:05:33 -0300

    subida professions

---
+++ src/routes/professions.js
@@ -0,0 +1,21 @@
+const { Router } = require('express')
+const router = Router()
+const { infoProfessions } = require('../controllers/professions')
+
+
+router.get('/', async (req, res, next)=>{
+    try {
+        let profsDb= await infoProfessions();
+        const filterDb= profsDb.map(p=>{
+                return{
+                    id: p.id,
+                    name: p.name
+                }
+            });
+        res.send(filterDb);
+    } catch (error) {
+        next(error)
+    }
+});
+
+module.exports = router;
