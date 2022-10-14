ffa7a104b4c6f44484e6d3a3be2ddc01d0cffa03
Joel301 <41971614+Joel301@users.noreply.github.com>
2022-10-13 21:48:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./src/app.js");
-
+const port = process.env.PORT || 3000;
 server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
 });
