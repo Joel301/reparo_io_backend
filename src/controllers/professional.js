c228d377ad1e25c7d98435ca7f83a142526a475b
Joel301 <joel3.1416@gmail.com>
2022-10-14 12:51:24 -0500

    relation profprof working

--- src/controllers/professional.js
+++ src/controllers/professional.js
@@ -1,16 +1,33 @@
-const { Professional } = require("../db.js");
+const { signedCookie } = require("cookie-parser");
+const { Professional, Profession } = require("../db.js");
 
 const getAllProfesional = async () => {
-    const results = await Professional.findAll();
+    const results = await Professional.findAll({
+        include: [{ model: Profession, exclude: ["Prof_Prof"] }],
+    });
     return results;
 };
 
 const postAProfesional = async (profesionalData) => {
+    const { professions, ...profesionalInfo } = profesionalData;
     if (!profesionalData.name) {
         return { error: "Profesional must have at least a name" };
     }
     try {
-        const newProfessional = await Professional.create(profesionalData);
+        const newProfessional = await Professional.create(profesionalInfo);
+        await Promise.all(
+            professions.map(async (p) => {
+                let profesion = {};
+                // esto es para tomar ambos sean objetos o id de profesion
+                isNaN(p)
+                    ? (profesion = await Profession.findOne({
+                          where: { name: p.id },
+                      }))
+                    : (profesion = await Profession.findByPk(p));
+                newProfessional.addProfession(profesion);
+            })
+        );
+        //para poder jalar las profesions se 
         return newProfessional.dataValues;
     } catch (e) {
         return { error: e };
