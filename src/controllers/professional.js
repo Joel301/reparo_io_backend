449ef70a868164c23b4c4d921b3629c3ce50fa11
armoraz <oawind@gmail.com>
2022-10-16 19:13:58 -0300

    cambiado para adaptar al modelo correcto

--- src/controllers/professional.js
+++ src/controllers/professional.js
@@ -1,58 +1,58 @@
 const { Professional, Profession } = require("../db.js");
 
 const getAllProfesional = async () => {
-    const results = await Professional.findAll();
-    return results;
+  const results = await Professional.findAll();
+  return results;
 };
 
 const infoById = async (id) => {
-    const profesionalId = await Professional.findOne({
-        where : { id: id },
-        include: [{model: Profession,
-        attributes: ["id","name"]}],
-    })
-    let {Professions} = profesionalId;
-    let profes = Professions ?
-    Professions.map((p) => p.name) : [];
+  const profesionalId = await Professional.findOne({
+    where: { id: id },
+    include: [{ model: Profession, attributes: ["id", "name"] }],
+  });
+  let { professions } = profesionalId;
+  let profes = professions ? professions.map((p) => p.name) : [];
 
-    const prof = {
-        id: profesionalId.id,
-        name: profesionalId.name,
-        profileImg: profesionalId.profileImg,
-        reputation: profesionalId.reputation,
-        professions: profes,
-    }
-    return prof
-}
+  const prof = {
+    id: profesionalId.id,
+    firstName: profesionalId.firstName,
+    lastName: profesionalId.lastName,
+    profileImg: profesionalId.profileImg,
+    reputation: profesionalId.reputation,
+    professions: profes,
+  };
+  return prof;
+};
 
 const postAProfesional = async (profesionalData) => {
-    if (!profesionalData.firstName) {
-        return { error: "Profesional must have at least a name" };
-    }
-    try {
-        const newProfessional = await Professional.create(profesionalData);
+  if (!profesionalData.firstName) {
+    return { error: "Profesional must have at least a name" };
+  }
+  try {
+    const newProfessional = await Professional.create(profesionalData);
 
-        await Promise.all(
-            profesionalData.professions.map(async (p) => {
-                let profesion = {};
-                // esto es para tomar ambos sean objetos o id de profesion
-                isNaN(p)
-                    ? (profesion = await Profession.findOne({
-                          where: { name: `${p}`},
-                      }))
-                    : (profesion = await Profession.findByPk(p));
-   
-                newProfessional.addProfession(profesion);
+    await Promise.all(
+      profesionalData.professions.map(async (p) => {
+        let profesion = {};
+        // esto es para tomar ambos sean objetos o id de profesion
+        isNaN(p)
+          ? (profesion = await Profession.findOne({
+              where: { name: `${p}` },
             }))
+          : (profesion = await Profession.findByPk(p));
 
-        return newProfessional.dataValues;
-    } catch (e) {
-        return { error: e };
-    }
+        newProfessional.addProfession(profesion);
+      })
+    );
+
+    return newProfessional.dataValues;
+  } catch (e) {
+    return { error: e };
+  }
 };
 
 module.exports = {
-    getAllProfesional,
-    postAProfesional,
-    infoById,
+  getAllProfesional,
+  postAProfesional,
+  infoById,
 };
