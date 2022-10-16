8f9eef0edd6308e285a7f076fc4e713ff4d00c5b
armoraz <oawind@gmail.com>
2022-10-16 13:36:56 -0300

    force:true

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const { conn } = require("./src/db.js");
 
 //cambiar mode para modificar modelos
 // mode = { force: true }
-mode = {alter:true};
+mode = {force:true};
 
 // Syncing all the models at once.
 conn.sync(mode).then(() => {
