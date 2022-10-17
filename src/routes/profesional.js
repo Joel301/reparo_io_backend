7b8b6843956f2da5f453f26d864ea430fb6c39f5
armoraz <oawind@gmail.com>
2022-10-17 19:49:08 -0300

    typo fixed

--- src/routes/profesional.js
+++ src/routes/profesional.js
@@ -1,52 +1,48 @@
 const { Router } = require("express");
 const {
-    getAllProfesional,
-    postAProfesional,
-    putProfesional,
-    delProfesional,
-    infoById,
+  getAllProfesional,
+  postAProfesional,
+  putProfesional,
+  delProfesional,
+  infoById,
 } = require("../controllers/professional");
 const router = Router();
 
 router.get("/", (req, res, next) => {
-    getAllProfesional().then((r) => res.send(r));
+  getAllProfesional().then((r) => res.send(r));
 });
-
-
 
 router.get("/:id", async (req, res, next) => {
-    const { id } = req.params
-    try {
-        const profesionalId = await infoById(id)
-        return res.status(200).send(profesionalId)
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.post("/", (req, res, next) => {
-
-    const { body } = req;
-    console.log(body, "aqui");
-    try{
-        await postAProfesional(body)
-        res.send({message:"agregado"})
-    } catch(e){
-        next(e)
-    }
-    
+  const { id } = req.params;
+  try {
+    const profesionalId = await infoById(id);
+    return res.status(200).send(profesionalId);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put('/:id', async (req, res, next)=>{
-    const {id} = req.params;
-    const { body } = req;
-    putProfesional(body,id).then((r) => console.log(r));
-    res.send(body);
+router.post("/", async (req, res, next) => {
+  const { body } = req;
+  console.log(body, "aqui");
+  try {
+    await postAProfesional(body);
+    res.send({ message: "agregado" });
+  } catch (e) {
+    next(e);
+  }
 });
-router.delete('/:id', async (req,res,next)=>{
-    const {id} = req.params;
-    delProfesional(id).then((r) => console.log(r));
-    res.send({message: "Professional deleted"});
+
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+  putProfesional(body, id).then((r) => console.log(r));
+  res.send(body);
+});
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  delProfesional(id).then((r) => console.log(r));
+  res.send({ message: "Professional deleted" });
 });
 
 module.exports = router;
