e2f6cf7cf5860126c3707a4fdbf10e975e90b907
Omar Perez <42899316+armoraz@users.noreply.github.com>
2022-10-24 21:15:10 -0300

    Cambiado a FLOAT

--- src/models/Order.js
+++ src/models/Order.js
@@ -3,7 +3,7 @@ const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("order", {
     amount: {
-      type: DataTypes.STRING,
+      type: DataTypes.FLOAT,
       allowNull: false,
     },
   });
