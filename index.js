e460365de629193153e590ee0409458b328d0a24
Omar Perez <42899316+armoraz@users.noreply.github.com>
2022-10-24 21:58:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const { conn } = require("./src/db.js");
 //cambiar mode para modificar modelos
 // mode = { force: true }
 
-mode = { alter: true };
+mode = { force: true };
 
 // Syncing all the models at once.
 conn.sync(mode).then(() => {
