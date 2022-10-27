4d9c847d3541239a5bf3465fe7bc031d342285d2
Joel301 <41971614+Joel301@users.noreply.github.com>
2022-10-26 19:24:23 -0500

    Update PostUser.js

--- src/controllers/PostUser.js
+++ src/controllers/PostUser.js
@@ -6,14 +6,12 @@ const { postAProfesional } = require("./professional");
 async function PostUser(userdata) {
     try {
         const { email, authid, isProfessional, isClient, ...data } = userdata
-        console.log(email, authid, isProfessional, isClient, data)
         const newUsuario = await User.create({ email, uid: authid })
         if (isProfessional) {
             const newProfessional = await postAProfesional({ ...data, email })
             await newUsuario.setProfessional(newProfessional)
         }
         if (isClient) {
-            console.log(userdata, data)
             const newClient = await postClient({ ...data, email })
             await newUsuario.setClient(newClient)
         }
