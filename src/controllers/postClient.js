5c175f87d270e63b7ee810a2fe02a504dbca283c
Joel301 <41971614+Joel301@users.noreply.github.com>
2022-10-26 19:25:10 -0500

    Update postClient.js

--- src/controllers/postClient.js
+++ src/controllers/postClient.js
@@ -3,12 +3,9 @@ const { isStringOk, isEmail } = require("../services/validaciones");
 
 async function postClient(data) {
     const { firstName, lastName, address, email } = data;
-    console.log('primerlog')
-    console.log(data)
     try {
         isStringOk([firstName, lastName, address]);
         isEmail(email);
-        console.log('llegueaqu')
         const newClient = await Client.create(data);
         return newClient
     } catch (e) {
