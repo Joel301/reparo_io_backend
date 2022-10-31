39d94b7f06d340c268610e0a6fe6ab310db95e9a
Joel301 <41971614+Joel301@users.noreply.github.com>
2022-10-31 16:23:27 -0600

    Update db.js
    cambiado de develop a main

--- src/db.js
+++ src/db.js
@@ -7,8 +7,8 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const sequelize = new Sequelize(
   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/reparoio`,
   //`postgres://postgres:RGdeXILyEnk1CVNUbrZW@containers-us-west-33.railway.app:6106/railway`,
-  "postgresql://postgres:nVqZlTmsw0QiBByyVOAD@containers-us-west-43.railway.app:6056/railway", //DEVELOP
-  // "postgresql://postgres:StAexDOXnSaL6lHRmIRM@containers-us-west-94.railway.app:5680/railway", //PRODUCCION
+  //"postgresql://postgres:nVqZlTmsw0QiBByyVOAD@containers-us-west-43.railway.app:6056/railway", //DEVELOP
+  "postgresql://postgres:StAexDOXnSaL6lHRmIRM@containers-us-west-94.railway.app:5680/railway", //PRODUCCION
 
   {
     logging: false, // set to console.log to see the raw SQL queries
