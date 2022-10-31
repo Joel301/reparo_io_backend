f109316e38ad19bd4ad56a23e07eda6974ffdeee
armoraz <oawind@gmail.com>
2022-10-30 21:49:47 -0300

    agregado controlador y ruta para obtener el carrito de compras del cliente

---
+++ src/controllers/getCartController.js
@@ -0,0 +1,23 @@
+const { Cart, CartDetail } = require("../db");
+
+const getCartController = async function (req, res, next) {
+  let cartId = req.params.id;
+
+  try {
+    const cart = await Cart.findOne({
+      where: { id: cartId },
+      include: CartDetail,
+    });
+
+    //Aplicando formato
+    res.json({
+      clientId: cart.clientId,
+      cartAmount: cart.amount,
+      items: cart.cartDetails,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getCartController;
