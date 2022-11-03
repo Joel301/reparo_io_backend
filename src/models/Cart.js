2fcc93728250b0ab2c12d3ab17334d2adcbdd734
Joel301 <joel3.1416@gmail.com>
2022-11-03 16:48:17 -0600

    added rating to geters and fix default new cart

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -2,20 +2,21 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
 
-    sequelize.define("cart", {
-        id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        },
-        amount: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-    },  
+  sequelize.define("cart", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    amount: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 0
+    },
+  },
     {
-        timestamps: true,
-        createdAt: false,
-      });
+      timestamps: true,
+      createdAt: false,
+    });
 };
 
