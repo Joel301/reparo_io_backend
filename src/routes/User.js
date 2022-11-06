7e9724a6e93ddf1dc0cff5b1a0566ef64219c8c2
hectordsol <hectordsol@gmail.com>
2022-11-06 06:37:50 -0300

    GET user query (id o email) return user and type user

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,21 +1,17 @@
 const { Router } = require('express')
-const getUserById = require('../controllers/GetUser')
+const { getUserById, getUserByEmail } = require('../controllers/GetUser')
 const PostUser = require('../controllers/PostUser')
 const router = Router()
 
 
 router.get("/", (req, res, next) => {
-    const { id } = req.query;
-    if (id) {
-        getUserById(id).then((user) => {
-            
-            res.send(user)
-        })
-    }
-    else {
-        res.send('thisisatestroute')
-    }
-
+    let id;let email;
+    req.query.id?id=req.query.id:null;//si viene id
+    req.query.email?email=req.query.email:null;//si viene email
+    console.log(req.query);
+    if (id) { getUserById(id).then((user) => { res.send(user) })}
+    else if (email) {getUserByEmail(email).then((user) => {res.send(user)})}
+    else { res.send('la pifiaste con la consulta') }
 })
 
 router.post("/", (req, res, next) => {
