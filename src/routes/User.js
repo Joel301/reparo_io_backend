4df975064d30321dcf26182b99c119e0c0d63ce8
Hector Dario SOL <95602009+hectordsol@users.noreply.github.com>
2022-11-06 06:42:21 -0300

    Update User.js

--- src/routes/User.js
+++ src/routes/User.js
@@ -8,7 +8,7 @@ router.get("/", (req, res, next) => {
     let id;let email;
     req.query.id?id=req.query.id:null;//si viene id
     req.query.email?email=req.query.email:null;//si viene email
-    console.log(req.query);
+
     if (id) { getUserById(id).then((user) => { res.send(user) })}
     else if (email) {getUserByEmail(email).then((user) => {res.send(user)})}
     else { res.send('la pifiaste con la consulta') }
