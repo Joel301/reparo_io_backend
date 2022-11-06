4bdb68222984beb15f3cfb8c2565b5d4cda086fd
armoraz <oawind@gmail.com>
2022-11-05 23:07:49 -0300

    trycatch agregado

--- src/routes/professions.js
+++ src/routes/professions.js
@@ -1,29 +1,34 @@
-const { Router } = require('express')
-const router = Router()
-const { infoProfessions, postProfessions } = require('../controllers/professions')
+const { Router } = require("express");
+const router = Router();
+const {
+  infoProfessions,
+  postProfessions,
+} = require("../controllers/professions");
 
-
-router.get('/', async (req, res, next) => {
-    try {
-        let profsDb = await infoProfessions();
-        res.send(profsDb);
-    } catch (error) {
-        next(error)
-    }
+router.get("/", async (req, res, next) => {
+  try {
+    let profsDb = await infoProfessions();
+    res.send(profsDb);
+  } catch (error) {
+    next(error);
+  }
 });
-router.post('/', async (req, res, next) => {
-    const { name } = req.body;
-    const { isnew, newProfessional, error } = await postProfessions({ name })
+router.post("/", async (req, res, next) => {
+  const { name } = req.body;
+  try {
+    const { isnew, newProfessional, error } = await postProfessions({ name });
     if (error) {
-        res.send({ error })
-        return
-    }
-    else if (!isnew) {
-        res.send({ message: "Ya existia", professional: newProfessional })
-        return
+      res.send({ error });
+      return;
+    } else if (!isnew) {
+      res.send({ message: "Ya existia", professional: newProfessional });
+      return;
     } else {
-        res.send(newProfessional)
+      res.send(newProfessional);
     }
-})
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
