d6ff8d8165e8f135f5490742e556acbec581dddb
armoraz <oawind@gmail.com>
2022-11-07 21:51:26 -0300

    typo

--- src/controllers/GetUser.js
+++ src/controllers/GetUser.js
@@ -1,21 +1,4 @@
 const { User, Professional, Admin, Client } = require("../db.js");
-
-async function getUserById(id) {
-  try {
-    const ID = id;
-    const currentUser = await User.findByPk(ID, {
-      include: [Professional, Admin, Client],
-    });
-    console.log(currentUser);
-    if (!currentUser) return { error: "not found" };
-    return currentUser;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-module.exports = { getUserById };
-const { User, Professional, Admin, Client } = require("../db.js");
 const getCartController = require("./getCartController.js");
 let isUUID =
   /[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}/gi;
