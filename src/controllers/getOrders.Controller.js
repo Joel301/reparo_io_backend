ca5e8b766616cccff8bfbacca12d2ef6e4be57d0
armoraz <oawind@gmail.com>
2022-11-08 07:59:37 -0300

    controlador de all orders

--- src/controllers/getOrders.Controller.js
+++ src/controllers/getOrders.Controller.js
@@ -1,5 +1,25 @@
 const { Order } = require("../db");
 
 const getOrdersController = async function (req, res, next) {
-  const orders = await Order.findAll();
+  try {
+    const ordersDB = await Order.findAll();
+    if (ordersDB) {
+      const orders = ordersDB.map((element) => {
+        return {
+          id: element.id,
+          amount: element.amount,
+          status: element.status,
+          clientId: element.clientId,
+        };
+      });
+
+      res.json(orders);
+    } else {
+      res.json({ msg: "No hay ordernes" });
+    }
+  } catch (error) {
+    next(error);
+  }
 };
+
+module.export = getOrdersController;
