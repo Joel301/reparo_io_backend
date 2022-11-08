c5344e739a2eab6563acf6f85b3595a6542c383b
hectordsol <hectordsol@gmail.com>
2022-11-08 12:20:00 -0300

    professions handel errors (posProfessions,infoProfessions)

--- src/controllers/professions.js
+++ src/controllers/professions.js
@@ -1,30 +1,39 @@
 const { Professional, Profession } = require('../db.js');
 
 const infoProfessions = async () => {
-  const profs = await Profession.findAll({
-    include: [
-      {
-        model: Professional,
-        //attributes: ["id"],
-      },
-    ],
-  });
-  return profs;
+  try{
+    const profs = await Profession.findAll({
+      include: [
+        {
+          model: Professional,
+          //attributes: ["id"],
+        },
+      ],
+    });
+    return profs;
+  }
+  catch (error){
+    res.status(500).send({ msg: "Error info professions (findAll)", error })
+  }
 };
 
 const postProfessions = async (data) => {
-  const { name } = data
-  const { length } = await infoProfessions()
-  if (!name) return { error: "es necesario el nombre del profesional" }
-  const [newProfessional, isnew] = await Profession.findOrCreate({
-    where: { name }, defaults: {
-      id: length + 1
-    }
-  });
-  console.log(isnew)
-  return { newProfessional, isnew }
+  const { name } = data;
+  try{
+    const { length } = await infoProfessions()
+    if (!name) return { error: "es necesario el nombre del profesional" }
+    const [newProfessional, isnew] = await Profession.findOrCreate({
+      where: { name }, defaults: {
+        id: length + 1
+      }
+    });
+    console.log(isnew)
+    return { newProfessional, isnew }
+  }
+  catch(error){
+    res.status(500).send({ msg: "Error post professions", error })
+  }
 }
-
 module.exports = {
   infoProfessions,
   postProfessions
