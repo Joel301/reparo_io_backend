924bc7973a51dafb0f9c9f848242a13ec249d322
armoraz <oawind@gmail.com>
2022-11-08 22:01:28 -0300

    Agregado servicio de email al cambiar estado de orden

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,9 +1,10 @@
 // Install with: npm install @trycourier/courier
 const { CourierClient } = require("@trycourier/courier");
 const { Client, Order } = require("../db");
+const { COURIER_TOKEN } = process.env;
 
 const courier = CourierClient({
-  authorizationToken: "pk_prod_RQGPZM7PF34F0QK42QNKNGDR4Z48",
+  authorizationToken: COURIER_TOKEN,
 });
 
 const sendOrderNotification = async function (clientId, orderId) {
@@ -31,6 +32,32 @@ const sendOrderNotification = async function (clientId, orderId) {
   });
 };
 
+const sendStatusChangeNotification = async function (
+  clientId,
+  orderId,
+  oldStatus
+) {
+  const client = await Client.findOne({ where: { id: clientId } });
+  const order = await Order.findOne({ where: { id: orderId } });
+
+  const { requestId } = await courier.send({
+    message: {
+      to: {
+        email: client.email,
+      },
+      template: "X5SXCPH6RPMHECMN0040JCX5P8TZ",
+      data: {
+        name:
+          client.firstName.charAt(0).toUpperCase() + client.firstName.slice(1),
+        orderId: order.id,
+        oldStatus,
+        status: order.status,
+      },
+    },
+  });
+};
+
 module.exports = {
   sendOrderNotification,
+  sendStatusChangeNotification,
 };
