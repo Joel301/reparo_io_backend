6bfa2c32b63b9b61c5f849d9ffa002809bb894bc
armoraz <oawind@gmail.com>
2022-11-10 01:05:11 -0300

    ruta para ordenes de profesional

--- src/routes/order.js
+++ src/routes/order.js
@@ -7,11 +7,13 @@ const removeOrderController = require("../controllers/removeOrderController");
 const removeOrderDetailController = require("../controllers/removeOrderDetailController");
 const getOrdersController = require("../controllers/getOrdersControllers");
 const updateOrderStatusController = require("../controllers/updateOrderStatusController");
+const getOrdersProfController = require("../controllers/getOrdersControllers");
 
 router.post("/", postOrderController);
 router.post("/test", postOrderDetails);
 router.get("/:id", getOrderIdController);
 router.get("/", getOrdersController);
+router.get("/professional/:id", getOrdersProfController);
 router.patch("/:id", updateOrderStatusController);
 router.delete("/:id", removeOrderController);
 router.delete("/order/:id", removeOrderDetailController);
